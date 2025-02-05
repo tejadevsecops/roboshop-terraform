4c137445737aac15e3ee7f7b69f946220694600b
Teja <awstheja727@gmail.com>
2025-02-05 15:27:22 +0530

    Added helm chart for nginx ingress controller

--- modules/eks/helm-charts.tf
+++ modules/eks/helm-charts.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    helm = {
-      source  = "hashicorp/helm"
-      version = "2.14.0"
-    }
-  }
-}
 resource "helm_release" "nginx-ingress" {
   chart = "oci://ghcr.io/nginx/charts/nginx-ingress"
   name  = "nginx-ingress"
--- provider.tf
+++ provider.tf
@@ -3,3 +3,9 @@ provider "vault" {
    token           = var.vault_token
    skip_tls_verify = true
  }
+
+provider "helm" {
+  kubernetes = {
+    config_path = "~/.kube/config"
+  }
+}
