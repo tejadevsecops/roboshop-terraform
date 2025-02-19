307cbed7c1d578ed772d30890eecec2f6dd671b3
Teja <awstheja727@gmail.com>
2025-02-19 21:17:00 +0530

    Roboshop terraform - Create a VPC

--- main.tf
+++ main.tf
@@ -1,15 +1,22 @@
-module "db_instances" {
-  for_each = var.db_instances
-  source = "./modules/ec2"
+module "vpc" {
+  for_each = var.vpc
   env = var.env
-  instance_type = each.value["instance_type"]
-  app_port = each.value["app_port"]
-  component_name = each.key
-  zone_id = var.zone_id
-  domain_name = var.domain_name
-  vault_token = var.vault_token
-  volume_size = each.value["volume_size"]
+  source = "./modules/vpc"
+  cidr_block = each.value["cidr_block"]
 }
+
+# module "db_instances" {
+#   for_each = var.db_instances
+#   source = "./modules/ec2"
+#   env = var.env
+#   instance_type = each.value["instance_type"]
+#   app_port = each.value["app_port"]
+#   component_name = each.key
+#   zone_id = var.zone_id
+#   domain_name = var.domain_name
+#   vault_token = var.vault_token
+#   volume_size = each.value["volume_size"]
+# }
 
 # module "app_instances" {
 #   depends_on = [module.db_instances]
@@ -39,11 +46,11 @@ module "db_instances" {
 #   volume_size = each.value["volume_size"]
 # }
 
-module "eks" {
-  source = "./modules/eks"
-  env = var.env
-  subnet_ids = var.eks["subnet_ids"]
-  addons     = var.eks["addons"]
-  node_groups = var.eks["node_groups"]
-  access_entries = var.eks["access_entries"]
-}
+# module "eks" {
+#   source = "./modules/eks"
+#   env = var.env
+#   subnet_ids = var.eks["subnet_ids"]
+#   addons     = var.eks["addons"]
+#   node_groups = var.eks["node_groups"]
+#   access_entries = var.eks["access_entries"]
+# }
--- variables.tf
+++ variables.tf
@@ -6,3 +6,4 @@ variable "zone_id" {}
 variable "domain_name" {}
 variable "vault_token" {}
 variable "eks" {}
+variable "vpc" {}
